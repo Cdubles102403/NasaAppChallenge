cd297795abbb3befcf0c87ee91d48b174f2598d8
Eric4106 <flappy4106@hotmail.com>
2020-11-18 10:07:39 -0500

    Final graphics and terrain polish

--- public/client.js
+++ public/client.js
@@ -7,12 +7,17 @@ var scl = 30;
 var cam;
 
 var terrain = [];
+var tMin = 1;
+var tMax = 0;
 
 var displayNormal = true;
 var displayHeight = false;
-var displayslope = false;
+var displaySlope = false;
 
 function setup() {
+  document.getElementById("Canvas").onwheel = function(event){
+    event.preventDefault();
+  };
 
   document.addEventListener("contextmenu",
     function(event) {
@@ -29,40 +34,54 @@ function setup() {
 
   cam = createCamera();
   camX = 0;
-  camY = 0;
+  camY = 150;
   camZ = 750;
   camDX = 0;
   camDY = 0;
   cam.setPosition(camX, camY, camZ);
-  cam.lookAt(0, 0, 0);
+  cam.lookAt(0, -300, 0);
 
-  cols = 96;
+  cols = 104;
   rows = 64;
   for(var x = 0; x <= rows; x++){
     terrain[x] = [];
     for(var y = 0; y < cols; y++){
-      terrain[x][y] = noise(x * .4, y * .4);
+      terrain[x][y] = noise(x * .25, y * .5);
+      if (terrain[x][y] < tMin) tMin = terrain[x][y];
+      if (terrain[x][y] > tMax) tMax = terrain[x][y];
     }
   }
 }
 
 function draw() {
+  colorMode(RGB);
   background(24);
   fill(255, 255, 255);
   translate((-cols * scl) / 2, (-rows * scl) / 2);
   for (var y = 0; y < rows - 1; y++) {
     beginShape(TRIANGLE_STRIP);
     for (var x = 0; x < cols; x++) {
-      fill(50 + terrain[y][x] * 100, 50 + terrain[y][x] * 100, 50 + terrain[y][x] * 100);
-      vertex(x * scl, y * scl,terrain[y][x] * 120);
-      vertex(x * scl, (y + 1) * scl,terrain[y+1][x] * 120);
+      if (displayNormal) {
+        fill(50 + terrain[y][x] * 100, 50 + terrain[y][x] * 100, 50 + terrain[y][x] * 100);
+      }
+      else if (displayHeight) {
+        colorMode(HSB);
+        fill(360 - (((terrain[y][x] - tMin) / (tMax - tMin * .85)) * 360), 100, 100);
+        console.log(terrain[y][x]);
+      }
+      else if (displaySlope) {
+        colorMode(HSB);
+
+      }
+      vertex(x * scl, y * scl, terrain[y][x] * 150);
+      vertex(x * scl, (y + 1) * scl, terrain[y+1][x] * 150);
     }
     endShape();
   }
-  rect(0, 0, cols * scl, rows * scl);
+  rect(0, 0, cols * scl, (rows - 1) * scl);
 
-  camX += camDX;
-  camY += camDY;
+  camX += camDX * ((camZ - 100) / 900);
+  camY += camDY * ((camZ - 100)/ 900);
 
   cam.setPosition(camX, camY, camZ);
 
@@ -117,12 +136,14 @@ function mouseDragged(event) {
 
 function keyPressed(event) {
   if (keyCode === 32) {
-    cam.lookAt(0, 0, 0);
+    //cam.lookAt(0, 0, 0);
   }
 }
 
 function mouseWheel(event) {
-  camZ += event.delta * 10;
+  if (event.target.nodeName === "CANVAS") {
+    camZ += event.delta * 10;
+  }
   if (camZ < 150) {
     camZ = 150;
   }
